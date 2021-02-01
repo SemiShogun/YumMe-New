ec6171bd9b436db380733bdd5f7880cb89702fda
SemiShogun <nectoj@gmail.com>
2021-02-01 16:32:20 +0100

    Add Navigation Drawer for easier navigation

---
+++ app/src/main/java/ch/zli/yumme/activities/HomeActivity.java
@@ -0,0 +1,66 @@
+package ch.zli.yumme.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+import ch.zli.yumme.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_home);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView) findViewById(R.id.nv);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.home:
+                        Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT);
+                        break;
+                    case R.id.add:
+                        Toast.makeText(HomeActivity.this, "Add Recipe", Toast.LENGTH_SHORT);
+                        break;
+                    default:
+                        return true;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
