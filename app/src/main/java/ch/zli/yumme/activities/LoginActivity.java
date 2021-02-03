bda848a4058a131852a0f6c89da8e16dc1763fb8
SemiShogun <nectoj@gmail.com>
2021-02-03 14:56:04 +0100

    Update LoginActivity layout values

--- app/src/main/java/ch/zli/yumme/activities/LoginActivity.java
+++ app/src/main/java/ch/zli/yumme/activities/LoginActivity.java
@@ -2,15 +2,15 @@ package ch.zli.yumme.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 import ch.zli.yumme.R;
 
@@ -28,8 +28,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
 
-        etEmail = findViewById(R.id.email);
-        etPassword = findViewById(R.id.password);
+        etEmail = findViewById(R.id.logEmail);
+        etPassword = findViewById(R.id.logPassword);
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
 
@@ -41,21 +41,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.login:
-                System.out.println(etEmail.getText().toString());
-                System.out.println(etPassword.getText().toString());
-                mAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                System.out.println("Logged in");
-                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                                startActivity(intent);
-                            } else {
-                                System.out.println("Error");
-                            }
-                        });
+                if (!etEmail.getText().toString().equals("") && !etPassword.getText().toString().equals("")) {
+                    mAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    System.out.println("Logged in");
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                                    Toast.makeText(LoginActivity.this, "Failed to login: " + e.getMessage(), Toast.LENGTH_SHORT);
+                                }
+                            });
+                } else {
+                    Toast.makeText(LoginActivity.this, "Email and password can't be empty", Toast.LENGTH_SHORT).show();
+                }
+
             case R.id.register:
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+            default:
+                break;
         }
     }
 }
