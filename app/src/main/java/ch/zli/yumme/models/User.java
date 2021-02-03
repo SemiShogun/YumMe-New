550fe3ccf0b8a79d91810243d2ee58fb0447ac6c
SemiShogun <nectoj@gmail.com>
2021-02-03 01:00:15 +0100

    Add User Class

--- app/src/main/java/ch/zli/yumme/models/User.java
+++ app/src/main/java/ch/zli/yumme/models/User.java
@@ -1,27 +1,64 @@
 package ch.zli.yumme.models;
 
 public class User {
-    private String email;
-    private String password;
+    private String username;
+    private String image;
+    private int followers;
+    private int following;
+    private int recipes;
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public User() {
+
     }
 
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getImage() {
+        return image;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(int followers) {
+        this.followers = followers;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(int following) {
+        this.following = following;
+    }
+
+    public int getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(int recipes) {
+        this.recipes = recipes;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", image='" + image + '\'' +
+                ", followers=" + followers +
+                ", following=" + following +
+                ", recipes=" + recipes +
+                '}';
     }
 }
