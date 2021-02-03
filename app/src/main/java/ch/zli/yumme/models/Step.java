20335d48abe9e45d1a1a55506cb160a1695a27c8
SemiShogun <nectoj@gmail.com>
2021-02-03 01:00:50 +0100

    Add Step Class

---
+++ app/src/main/java/ch/zli/yumme/models/Step.java
@@ -0,0 +1,17 @@
+package ch.zli.yumme.models;
+
+public class Step {
+    private String instruction;
+
+    public Step(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+}
